26ea7b439f425561d4837d975f5d4cab4ba5b72c
sw19_sungpil <51325183+popopy0412@users.noreply.github.com>
2021-11-26 17:02:12 +0900

    Update ImageTest.java

--- WoW/ImageTest.java
+++ WoW/ImageTest.java
@@ -7,6 +7,7 @@ public class ImageTest extends JFrame{
     private Image image = icon.getImage(); // 이미지 가져옴
     private MyPanel panel = new MyPanel(); // 그림을 그릴 패널
     private int X, Y; // 사진의 너비와 높이
+    private int pX, pY; // 프레임의 너비와 높이
     public ImageTest(String title){
         super(title); // 제목 설정
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // 종료 조건 설정
@@ -18,10 +19,11 @@ public class ImageTest extends JFrame{
     class MyPanel extends JPanel{
         public void paintComponent(Graphics g){
             super.paintComponent(g);
-            g.drawImage(image, 0, 0, X/2, Y/2, 0, 0, X/2, Y/2, this);
-            g.drawImage(image, X/2+10, 0, X+10, Y/2, X/2, 0, X, Y/2, this);
-            g.drawImage(image, 0, Y/2+10, X/2, Y+10, 0, Y/2, X/2, Y, this);
-            g.drawImage(image, X/2+10, Y/2+10, X+10, Y+10, X/2, Y/2, X, Y, this); // 사진 그리기
+            pX = getWidth(); pY = getHeight();
+            g.drawImage(image, 0, 0, pX/2, pY/2, 0, 0, X/2, Y/2, this);
+            g.drawImage(image, pX/2+10, 0, pX+10, pY/2, X/2, 0, X, Y/2, this);
+            g.drawImage(image, 0, pY/2+10, pX/2, pY+10, 0, Y/2, X/2, Y, this);
+            g.drawImage(image, pX/2+10, pY/2+10, pX+10, pY+10, X/2, Y/2, X, Y, this); // 사진 그리기
         }
     }
     public static void main(String[] args){
